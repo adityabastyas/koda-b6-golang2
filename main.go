40346d28d3fc68a6160530285a658c81c2cd7ed2
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-02-18 01:47:29 +0700

    fix: uncoment in func main

--- main.go
+++ main.go
@@ -1,23 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
 	Slice()
 
-	// scores := []int {50,75,66,20,32,90}
+	scores := []int{50, 75, 66, 20, 32, 90}
 
-	// posisi := 0
+	posisi := 0
 
-	// for x:=range scores{
-	// 	if scores[x] == 66 {
-	// 		posisi = x
-	// 	}
-	// }
+	for x := range scores {
+		if scores[x] == 66 {
+			posisi = x
+		}
+	}
 
-	// kiri:= scores[:posisi + 1]
-	// kanan := scores[posisi + 1 :]
+	kiri := scores[:posisi+1]
+	kanan := scores[posisi+1:]
+	// fmt.Println(kanan)
 
-	// // scores = append(kiri, append(88) kanan...)
-	// scores = append(kiri, append([]int{88}, kanan...)...)
+	// scores = append(kiri, append(88) kanan...)
+	scores = append(kiri, append([]int{88}, kanan...)...)
 
-	// fmt.Println(scores)
+	fmt.Println(scores)
 }
